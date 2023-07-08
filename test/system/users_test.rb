0852e1231d8d4dc6005e9592346028da605086a9
kerni2 <kerni2@ya.ru>
2023-07-08 03:37:42 +0300

    add test that user can update their time_zone

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -1,6 +1,7 @@
 require "application_system_test_case"
 
 class UsersTest < ApplicationSystemTestCase
+  include Devise::Test::IntegrationHelpers
   def setup
     @user = users(:confirmed_user)
   end
@@ -9,9 +10,9 @@ class UsersTest < ApplicationSystemTestCase
     visit new_user_registration_path
   
     assert_difference("User.count") do
-      fill_in "Email", with: "user2@mail.com"
+      fill_in "Email Address", with: "user2@mail.com"
       fill_in "Password", with: "password"
-      fill_in "Password confirmation", with: "password"
+      fill_in "Confirm Password", with: "password"
 
       click_button "Sign up"
     end
@@ -20,10 +21,24 @@ class UsersTest < ApplicationSystemTestCase
   test "should sign in" do
     visit new_user_session_path
 
-    fill_in "Email", with: @user.email
+    fill_in "Email Address", with: @user.email
     fill_in "Password", with: "password"
     click_button "Log in"
 
     assert_selector "p", text: "Signed in successfully."
   end
+
+  test "should update time_zone" do
+    sign_in @user
+
+    visit edit_user_registration_path
+
+    select 'London'
+    fill_in "Current Password", with: "password"
+
+    click_button "Save Changes"
+
+    assert_equal 'London', @user.reload.time_zone
+  end
 end
+
