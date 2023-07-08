1f35e37a2a339ae5a7bdf09a6f1a6cf618edabdd
kerni2 <kerni2@ya.ru>
2023-07-08 04:22:53 +0300

    add application time_zone to current_user.time_zone

---
+++ test/integration/time_zone_test.rb
@@ -0,0 +1,15 @@
+require "test_helper"
+
+class TimeZoneTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+  def setup
+    @user = users(:confirmed_user_with_time_zone)
+  end
+
+  test "should set application time_zone to equal user.time_zone" do
+    sign_in @user
+
+    get root_path
+    assert_equal 'Berlin', Time.zone.name
+  end
+end
