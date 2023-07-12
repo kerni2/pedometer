0ac5209a0530b7abe8b98eb5b800cd83242e2b93
kerni2 <kerni2@ya.ru>
2023-07-12 05:06:03 +0300

    add activities table and associate with users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_05_015822) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_12_020000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "activities", force: :cascade do |t|
+    t.integer "duration"
+    t.integer "category", default: 0, null: false
+    t.decimal "distance", precision: 5, scale: 2
+    t.integer "difficulty"
+    t.integer "unit"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_activities_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,4 +44,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_05_015822) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "activities", "users"
 end
