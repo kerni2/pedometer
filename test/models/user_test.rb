d45600a441df5906078752a03a20434244dbe71c
kerni2 <kerni2@ya.ru>
2023-07-15 08:02:47 +0300

    add validations for Activity model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 class UserTest < ActiveSupport::TestCase
   def setup
     @user = User.new(email: "user@mail.com", password: "password", password_confirmation: "password")
+    @user_with_activities = users(:confirmed_user_with_activities)
   end
 
   test "should be valid" do
@@ -14,4 +15,9 @@ class UserTest < ActiveSupport::TestCase
     assert_equal "UTC", @user.reload.time_zone
   end
 
+  test "should delete associated activities" do
+    assert_difference("Activity.count", -"#{@user_with_activities.activities.count}".to_i) do
+      @user_with_activities.destroy
+    end
+  end
 end
