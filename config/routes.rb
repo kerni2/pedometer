40efae06e375829f0916e98bdc5145c71ecf9363
kerni2 <kerni2@ya.ru>
2023-08-09 19:36:56 +0300

    add route for authenticated user

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  authenticated :user do
+    root 'activities#index', as: :authenticated_root
+  end
+
+  root 'static_pages#home'
+  
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
 
-  root 'static_pages#home'
   resources :activities
 end
