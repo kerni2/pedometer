94d8b536fe091383c3e993c9517aa81dcd6e254e
kerni2 <kerni2@ya.ru>
2023-08-13 10:02:01 +0300

    add total model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_04_051404) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_13_065843) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -69,6 +69,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_04_051404) do
     t.index ["user_id"], name: "index_activities_on_user_id"
   end
 
+  create_table "totals", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.date "date", null: false
+    t.integer "duration", default: 0
+    t.decimal "distance", default: "0.0"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_totals_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -90,4 +100,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_04_051404) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "activities", "users"
+  add_foreign_key "totals", "users"
 end
