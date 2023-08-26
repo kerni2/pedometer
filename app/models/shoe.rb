5465981c0dcc7750e08bc2c38f1808ffde9b3da8
kerni2 <kerni2@ya.ru>
2023-08-26 08:52:06 +0300

    add test to shoe model

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -1,4 +1,7 @@
 class Shoe < ApplicationRecord
   belongs_to :activity, optional: true
   belongs_to :user
+
+  validates :name, presence: true
+  validates :allowed_distance_in_miles, numericality: { only_integer: true, greater_than_or_equal_to: 1, allow_nil: true }
 end
